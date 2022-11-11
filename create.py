25e45d2b64277643121bb02d54d64631b53acd82
ivanmttc <113404353+ivanmttc@users.noreply.github.com>
2022-11-11 22:57:17 +0800

    Add files via upload

--- create.py
+++ create.py
@@ -6,40 +6,54 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-docs = [
+doc = [
 {
-  "name": "陳武林",
-  "mail": "wlchen@pu.edu.tw",
-  "lab": 665
+	"Code": "3421",
+	"Course": "行銷管理",
+	"Leacture": "顏永森、康贊清",
+	"Time": "四2、3、4",
+	"Room":"主顧217"
 },
-
 {
-  "name": "莊育維",
-  "mail": "ywchuang@pu.edu.tw",
-  "lab": 566
+	"Code": "3422",
+	"Course": "程式語言",
+	"Leacture": "鄭婉淑",
+	"Time": "五2、3、4",
+	"Room":"計206"
 },
-
 {
-  "name": "汪于茵",
-  "mail": "yywang13@pu.edu.tw",
-  "lab": 674
+	"Code": "3423",
+	"Course": "行動電子商務",
+	"Leacture": "康贊清",
+	"Time": "三7、8、9",
+	"Room":"主顧303"
+},
+{
+	"Code": "3424",
+	"Course": "網頁前端程式設計",
+	"Leacture": "胡育誠",
+	"Time": "三2、3、4",
+	"Room":"主顧322"
+},
+{
+	"Code": "3425",
+	"Course": "物聯網概論",
+	"Leacture": "王耀德",
+	"Time": "三7、8、9",
+	"Room":"主顧324"
+},
+{
+	"Code": "3466",
+	"Course": "大學入門",
+	"Leacture": "邱梓綸",
+	"Time": "一8、9",
+	"Room":"靜宜大學"
 }
-
 ]
 
-collection_ref = db.collection("靜宜資管")
-for doc in docs:
-  collection_ref.add(doc)
-
-
-doc = {
-  "name": "莫天賜",
-  "mail": "ivan19982008@gmail.com",
-  "lab": 579
-}
-
-#doc_ref = db.collection("靜宜資管").document("tcyang")
+#doc_ref = db.collection("111").document("3420")
 #doc_ref.set(doc)
 
-collection_ref = db.collection("靜宜資管")
-collection_ref.add(doc)
+collection_ref = db.collection("111")
+for doc in doc:
+	collection_ref.add(doc)
