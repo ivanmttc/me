bd975c323f2d6a250d5aa76f9097e1f379b51b9b
ivanmttc <113404353+ivanmttc@users.noreply.github.com>
2022-11-11 23:20:55 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -5,41 +5,55 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 from flask import Flask, render_template, request
-from datetime import datetime,timezone,timedelta
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>莫天賜Python網頁</h1>"
-    homepage += "<br><a href=/search>選修課程查詢</a><br>"
+    homepage = "<h1>莫天賜個人網頁</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/about>莫天賜簡介網頁</a><br>"
+    homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
+    homepage += "<a href=/work>工作需求</a><br>"
     return homepage
 
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
 
 
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    if request.method == "POST":
-        cond = request.form["keyword"]
-        result = "請輸入您要查詢的課程關鍵字：" + cond
+@app.route("/about")
+def account():
+    
+        return render_template("about.html")
 
-        db = firestore.client()
-        collection_ref = db.collection("111")
-        docs = collection_ref.get()
-        result = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if cond in dict["Course"]:
-                #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
-                result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
-                result += dict["Time"] + "於" + dict["Room"] + "上課<br>"
-        print(result)
-        if result == "":
-            result = "sorry....."
-        return result
-    else:
-        return render_template("search.html")
+@app.route("/work")
+def work():
+    
+        return render_template("work.html")
+        
+@app.route("/read")
+def read():
+    Result = ""     
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.order_by("mail", direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#   app.run()
+
